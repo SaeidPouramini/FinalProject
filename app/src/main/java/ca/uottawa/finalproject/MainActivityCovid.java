7fc0adfd72115f9809adb9b350944c615405163b
matin <behz0003@algonquinlive.com>
2020-12-07 00:15:30 -0500

    saeed

--- app/src/main/java/ca/uottawa/finalproject/MainActivityCovid.java
+++ app/src/main/java/ca/uottawa/finalproject/MainActivityCovid.java
@@ -11,7 +11,7 @@ public class MainActivityCovid extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_covid);
 
         findViewById(R.id.covid19CaseData).setOnClickListener(v -> {
             startActivity(new Intent(MainActivityCovid.this, Covid19CountryEnterActivity.class));
