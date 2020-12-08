01ce6806c4516a528152ab54ac9db741fa0992b3
sweet <sweetlife_1998@hotmail.com>
2020-12-07 20:01:38 -0500

    Master commit 2

--- app/src/main/java/ca/uottawa/finalproject/MainActivity.java
+++ app/src/main/java/ca/uottawa/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package ca.uottawa.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.covidbutton).setOnClickListener(v -> {
+                    startActivity(new Intent(MainActivity.this, MainActivityCovid.class));
+        });
+        findViewById(R.id.Recipebutton2).setOnClickListener(v -> {
+                    startActivity(new Intent(MainActivity.this, Recipe_activity.class));
 
+        });
+                findViewById(R.id.audiobutton3).setOnClickListener(v -> {
+                    startActivity(new Intent(MainActivity.this, MainActivityAudio.class));
+                });
+                    findViewById(R.id.ticketbutton4).setOnClickListener(v -> {
+                        startActivity(new Intent(MainActivity.this, TicketMasterEventSearchActivity.class));
+                    });
     }
 }
